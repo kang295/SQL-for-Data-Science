a82f8d8b5f9ba2628d1d58ca7b3d33abb5649a47
HYOJU KANG <71005886+kang295@users.noreply.github.com>
2023-01-21 20:49:36 -0500

    Update Quiz3.sql

--- Quiz3.sql
+++ Quiz3.sql
@@ -49,3 +49,81 @@ invoices.InvoiceId
 FROM invoices
 
 CROSS JOIN invoice_items
+
+-- Using a subquery, find the names of all the tracks for the album "Californication".
+SELECT 
+Name,
+AlbumId
+FROM Tracks
+WHERE AlbumId IN 
+(SELECT AlbumId 
+FROM Albums
+WHERE Title = 'Californication')
+
+-- Find the total number of invoices for each customer along with the customer's full name, city and email.
+SELECT 
+FirstName,
+LastName,
+City,
+Email,
+COUNT(C.CustomerId) AS Invoices
+FROM Customers C INNER JOIN Invoices I
+ON C.CustomerId = I.CustomerId
+GROUP BY C.CustomerId
+
+-- Retrieve the track name, album, artistID, and trackID for all the albums.
+SELECT
+Albums.Title AS Album,
+Artists.Name AS Artist,
+Tracks.Name,
+Tracks.TrackId
+
+
+FROM((Tracks INNER JOIN Albums
+ON Albums.AlbumId = Tracks.AlbumId)
+INNER JOIN Artists
+ON Artists.ArtistId = Albums.ArtistId)
+
+-- Retrieve a list with the managers last name, and the last name of the employees who report to him or her.
+SELECT
+M.LastName AS Manager,
+E.LastName AS Employee
+FROM Employees E 
+INNER JOIN Employees M
+ON E.ReportsTo = M.EmployeeId
+
+-- Find the name and ID of the artists who do not have albums. 
+SELECT
+Albums.Title AS Album,
+Artists.ArtistId,
+Name AS Artist
+FROM Artists LEFT JOIN Albums
+ON Artists.ArtistId = Albums.ArtistId
+WHERE Album IS NULL
+
+-- Use a UNION to create a list of all the employee's and customer's first names and last names ordered by the last name in descending order.
+SELECT
+FirstName,
+LastName
+FROM Employees
+
+UNION
+
+SELECT
+FirstName,
+LastName
+FROM Customers
+ORDER BY LastName DESC
+
+-- See if there are any customers who have a different city listed in their billing city versus their customer city.
+SELECT
+C.FirstName,
+C.LastName,
+C.City AS CustomerCity,
+I.BillingCity
+FROM Customers C
+INNER JOIN Invoices I
+ON C.CustomerId = I.CustomerId
+
+Where CustomerCity != BillingCity
+
